ade4cbe50bddf0bc53bb2d539e1d758181505eaa
Ayman Abdelfattah <56743254+Ayman1242@users.noreply.github.com>
2023-03-15 01:42:58 -0600

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,28 @@
+export const projects = [
+  {
+    title: "Appraisal Android App",
+    subtitle: "Java",
+    description:
+      "An application for hosting experiments as well as conducting and submitting trials. Created through Android Studio and uses Firebase DBMS.",
+    image: "./Appraisal.jpg",
+    link: "https://github.com/CMPUT301W21T11/Appraisal",
+    
+  },
+  {
+    title: "Cognitive Test Plugin",
+    subtitle: "PHP and JS",
+    description:
+      "Moodle Plugin created for piloting students which train and test piloting students in skills such as memory, multitasking, and vigilance.",
+    image: "./s5GBRkz.png",
+    link: "https://cmput401.ca/projects/36ef9bb9-ff57-43cc-b8d8-264534e33097",
+  },
+];
+
+export const skills = [
+  "Python",
+  "React",
+  "SQL",
+  "C/C++/C#",
+  "Git",
+  "Unit Testing",
+];
